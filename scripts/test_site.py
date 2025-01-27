493f0a64b3b8ccbdace15870663542012dbb18ee
Sahil Kale <sahil.kale1@gmail.com>
2025-01-26 22:15:00 -0500

    Initial Commit

---
+++ scripts/test_site.py
@@ -0,0 +1,14 @@
+import subprocess
+
+if __name__ == '__main__':
+    """Deploy the site for temp use"""
+    cmd = "bundle exec jekyll serve"
+
+    SITE_DIR = "circuits-and-code-website"
+
+    try:
+        print(f"Running command: {cmd}")
+        subprocess.run(cmd, shell=True, check=True, cwd=SITE_DIR)
+    except subprocess.CalledProcessError as e:
+        print(e)
+        exit(1)
